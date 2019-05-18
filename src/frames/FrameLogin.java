d6175971b091dca00e74ff04f96ab7ce7a037cfe
Damien Beck <damien.becque2nd8@gmail.com>
2019-05-18 20:22:41 +0200

    add reader and change frames

--- src/frames/FrameLogin.java
+++ src/frames/FrameLogin.java
@@ -6,21 +6,12 @@ import java.util.OptionalLong;
 
 public class FrameLogin implements Frame {
 
-<<<<<<< HEAD
     private final byte opcode=0;
     private final String login;
 
     public FrameLogin(String login) {
         this.login = login;
     }
-=======
-    private final byte opcode = 0;
-    private final String login;
-    
-    public FrameLogin(String login) {
-		this.login = login;
-	}
->>>>>>> f6814e79a04d9f605e3f23fbbccc54d45155621a
 
     @Override
     public byte getOpcode() {
@@ -29,11 +20,7 @@ public class FrameLogin implements Frame {
 
     @Override
     public Optional<String> getLoginSender(){
-<<<<<<< HEAD
         return Optional.ofNullable(login);
-=======
-        return Optional.of(login);
->>>>>>> f6814e79a04d9f605e3f23fbbccc54d45155621a
     }
 
     @Override
@@ -52,19 +39,11 @@ public class FrameLogin implements Frame {
     }
 
     @Override
-<<<<<<< HEAD
-    public ByteBuffer getBuffer() {
+    public ByteBuffer asBuffer() {
         ByteBuffer log= StringToBbManager.stringToBB(login);
         ByteBuffer toRet= ByteBuffer.allocate(1+log.remaining());
         toRet.put(opcode);
         toRet.put(log);
         return toRet.flip();
-=======
-    public ByteBuffer asBuffer() {
-        return ByteBuffer.allocate(Byte.BYTES)
-        		.put(opcode)
-        		.put(StringToBbManager.stringToBB(login))
-        		.flip();
->>>>>>> f6814e79a04d9f605e3f23fbbccc54d45155621a
     }
 }
